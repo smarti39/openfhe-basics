0897feb2c12621aeb65b1355d34ae683cb422648
Helena Berens <hberens@tjws-03.cse.nd.edu>
2024-12-06 14:23:38 -0500

    more updates

--- src/main.cpp
+++ src/main.cpp
@@ -85,28 +85,30 @@ int main(int argc, char *argv[]) {
   // dbVectors is 2-D of size 512x1024, all vectors already normalized
 
   printMatrix(dbVectors);
+  std::cout << std::endl;
   std::vector<std::vector<std::vector<double>>> squareMatrices = splitIntoSquareMatrices(dbVectors, VECTOR_DIM);
-  //for (auto &matrix: squareMatrices) { 
-  //    printMatrix(matrix);
-        //std::cout << std:endl;
-  //}
+  /*for (auto &matrix: squareMatrices) { 
+      printMatrix(matrix);
+      std::cout << std::endl;
+  }*/
   
   std::vector<std::vector<std::vector<double>>> allDiagonalMatrices;
   
   for (auto &squareMatrix: squareMatrices) {
       std::vector<std::vector<double>> diagonals = preprocessToDiagonalForm(squareMatrix);
+      //printMatrix(diagonals);
       allDiagonalMatrices.push_back(diagonals);
   }
 
   std::vector<std::vector<double>> concatenatedRows = concatenateRows(allDiagonalMatrices);
-  /*std::cout << "\nConcatenated Rows:" << std::endl;
+  std::cout << "\nConcatenated Rows:" << std::endl;
     for (const auto& row : concatenatedRows) {
-        for (int value : row) {
+        for (double value : row) {
             std::cout << value << " ";
         }
         std::cout << std::endl;
         std::cout << std::endl;
-    }*/
+    }
 
   // Compute the matrix-vector product of dbVectors times queryVector in the encrypted domain
   cout << "Beginning implementation..." << endl;
